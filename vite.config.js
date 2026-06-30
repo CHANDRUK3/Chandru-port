a67ed5f160cb56af713702418e4b1067bea11d0e
chandruk0626 <your_github_email@example.com>
2026-06-30 09:53:05 +0530

    Configure for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Chandru-port/',
+  base: '/',
   plugins: [react()],
   build: {
     minify: 'terser',
